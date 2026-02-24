6f1be3087885f748a277e3f3bbfa573e601c8dc9
joaquin (Pato) Decima <jdecima@vasak.net.ar>
2026-02-24 17:39:09 -0300

    feat: agregar soporte para i18n con tauri-plugin-i18n y actualizar dependencias

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -8,6 +8,7 @@ pub fn run() {
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_config_manager::init())
         .plugin(tauri_plugin_vicons::init())
+        .plugin(tauri_plugin_i18n_vsk::init(None))
         .invoke_handler(tauri::generate_handler![
             icons::get_icon_base64,
             icons::get_symbol_base64
