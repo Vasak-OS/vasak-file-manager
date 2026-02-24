f0b417588ec6f1efc6929ed559f670e8db181fb6
joaquin (Pato) Decima <jdecima@vasak.net.ar>
2026-02-24 16:03:30 -0300

    feat: agregar tauri-plugin-store y actualizar configuraciones de permisos

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,6 +4,7 @@ mod icons;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_config_manager::init())
         .plugin(tauri_plugin_vicons::init())
