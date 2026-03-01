3f9e5fb98a800f4d804371ae90e2c23e129f4333
joaquin (Pato) Decima <jdecima@vasak.net.ar>
2026-03-01 13:37:34 -0300

    feat: agregar archivo CSS principal y actualizar importación en main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createPinia } from 'pinia';
 import { createApp } from 'vue';
 import I18n from '@vasakgroup/tauri-plugin-i18n';
 import App from '@/App.vue';
-import '@/style.css';
+import '@/assets/main.css';
 
 const i18n = I18n.getInstance()
 const app = createApp(App);
